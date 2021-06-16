b4896aea7b1d95eafcfc82d782ee6c74bf64b2c4
rasti-najim <rastirasheed@gmail.com>
2021-06-16 20:43:17 +0300

    added env file

--- app.js
+++ app.js
@@ -7,6 +7,28 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// * get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await pool.query("SELECT * FROM todos");
+    res.send(todos.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// * get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const todo = await pool.query("SELECT * FROM todos WHERE todo_id = $1", [
+      req.params.id,
+    ]);
+    res.send(todo.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 // * create a todo
 app.post("/todos", async (req, res) => {
   try {
@@ -22,6 +44,36 @@ app.post("/todos", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Listening on port 5000...");
+// * update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const todo = await pool.query(
+      "UPDATE todos SET description = $1 WHERE todo_id = $2 RETURNING *",
+      [description, id]
+    );
+    res.send(todo.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+// * delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query(
+      "DELETE FROM todos WHERE todo_id = $1 RETURNING *",
+      [id]
+    );
+    res.send(todo.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
--- db.js
+++ db.js
@@ -1,11 +1,28 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
-const pool = new Pool({
-  user: "postgres",
-  password: "*3EyDeeKYso^^GQs!XuSsqjf#UCHPP",
-  host: "localhost",
-  port: 5432,
-  database: "todo_app",
-});
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE,
+};
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL, // heroku addon
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
+
+// const pool = new Pool({
+//   user: "postgres",
+//   password: "*3EyDeeKYso^^GQs!XuSsqjf#UCHPP",
+//   host: "localhost",
+//   port: 5432,
+//   database: "todo_app",
+// });
 
 module.exports = pool;
